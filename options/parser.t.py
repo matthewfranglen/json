7f12aac6efb3c6ac45190a219a18c24de6acf021
Matthew Franglen <matthew@franglen.org>
2013-03-24 14:40:10 +0000

    More tests, added a common method for testing

--- options/parser.t.py
+++ options/parser.t.py
@@ -2,15 +2,25 @@ from parser import parse_arguments
 import unittest
 
 class TestParser(unittest.TestCase):
+    def _test(self, string, expected):
+        tokens = parse_arguments(string)
+        self.assertEqual(tokens, expected)
+
     def test_string(self):
-       tokens = parse_arguments("'hello'")
-       self.assertEqual(tokens, [("'hello'", 'string')])
+       self._test("'hello'",               [("'hello'", 'string')])
+       self._test('"hello"',               [('"hello"', 'string')])
+       self._test("'hello' \"world\"",     [("'hello'", 'string'), ('"world"', 'string')])
+       self._test("'hello \\\'world\\\''", [("'hello \\\'world\\\''", 'string')])
 
     def test_number(self):
         pass
 
     def test_token(self):
-        pass
+        self._test("[1, 2, 3]",                                [("[1, 2, 3]", 'token')])
+        self._test("{ 'key': 'value' }",                       [("{ 'key': 'value' }", 'token')])
+        self._test("{ 'key': [1, 2, 3] }",                     [("{ 'key': [1, 2, 3] }", 'token')])
+        self._test("{ 'key': [1, 2, { 'key': 'value' } ] }",   [("{ 'key': [1, 2, { 'key': 'value' } ] }", 'token')])
+        self._test("[1, 2, 3] { 'key': 'value' } [ 4, 5, 6 ]", [("[1, 2, 3]", 'token'),("{ 'key': 'value' }", 'token'),("[ 4, 5, 6 ]", 'token')])
 
     def test_operator(self):
         pass
