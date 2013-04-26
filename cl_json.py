3713a7615dbcafdd76f72bdd9bfde0a43c77e74b
Matthew Franglen <matthew@franglen.org>
2013-04-26 23:31:39 +0100

    Shows help when no rules specified

--- cl_json.py
+++ cl_json.py
@@ -8,23 +8,20 @@ def help():
     Displays usage for this tool, with examples.
     """
     print """
-json path
+cat file | json path
 
 This prints all the values at path for the json provided. The json will come
 from standard in.
 
-The path allows you to specify what you are interested in. You do this by
-specifying a list of operators that must successfully apply, in a manner
-similar to css.
-
-Some operators apply to the data passed in, while others apply to operators.
-When a data operator depends on a specific type of data, such as looking up an
-index in an array, that operator will reject all data that is not of that form.
+See the README.md for syntax and examples.
 """
 
 if __name__ == "__main__":
-    json   = parse_json(stdin.read())
-    rules  = parse_arguments(" ".join(argv[1:]))
-    result = visit(rules, json)
+    if len(argv) < 2:
+        help()
+    else:
+        json   = parse_json(stdin.read())
+        rules  = parse_arguments(" ".join(argv[1:]))
+        result = visit(rules, json)
 
-    print result
+        print result
