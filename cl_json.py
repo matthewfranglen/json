7278f1951d8d650f9b9158f08ddac2522d86df4c
Matthew Franglen <matthew@franglen.org>
2013-04-26 23:27:39 +0100

    cl_json.py now working
    Currently no way to display the help.

--- cl_json.py
+++ cl_json.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python
-import json
+from options.parser  import parse_json, parse_arguments
+from options.visitor import visit
+from sys             import argv, stdin
 
-def help:
+def help():
     """
     Displays usage for this tool, with examples.
     """
     print """
-json path [file, file, file...]
+json path
 
-This prints all the values at path for the json provided. The json will come from standard in as well as any specified files.
+This prints all the values at path for the json provided. The json will come
+from standard in.
 
-The path allows you to specify what you are interested in. You do this by specifying a list of operators that must successfully apply, in a manner similar to css.
+The path allows you to specify what you are interested in. You do this by
+specifying a list of operators that must successfully apply, in a manner
+similar to css.
 
-Some operators apply to the data passed in, while others apply to operators. When a data operator depends on a specific type of data, such as looking up an index in an array, that operator will reject all data that is not of that form.
+Some operators apply to the data passed in, while others apply to operators.
+When a data operator depends on a specific type of data, such as looking up an
+index in an array, that operator will reject all data that is not of that form.
 """
 
 if __name__ == "__main__":
-    pass
+    json   = parse_json(stdin.read())
+    rules  = parse_arguments(" ".join(argv[1:]))
+    result = visit(rules, json)
+
+    print result
